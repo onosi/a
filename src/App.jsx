5763a092a3ea5a9b33c371fb4c46f6ac07212fd9
onosi <32011068+onosi@users.noreply.github.com>
2022-01-23 23:35:05 +0900

    feat: create add function

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,40 @@ import { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
-  const [imcompleteTodos, setIncompleteTodos] = useState([
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいい"
   ]);
   const [completeTodos, setcompleteTodos] = useState(["うううう"]);
+
+  const onChangeTodoText = (event) => {
+    setTodoText(event.target.value);
+  };
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {imcompleteTodos.map((imcompleteTodo) => {
+          {incompleteTodos.map((incompleteTodo) => {
             return (
-              <div key={imcompleteTodo} className="list-row">
-                <li>{imcompleteTodo}</li>
+              <div key={incompleteTodo} className="list-row">
+                <li>{incompleteTodo}</li>
                 <button>完了</button>
                 <button>削除</button>
               </div>
